e04542da1ea39e3c27355ec4fda0e99f82da10d5
cbailey03 <cbailey@lightsett.com>
2023-11-08 08:28:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ for row in range(start_row, end_row+1):
     zip_code = ws_our_data.cell(row=row,column=5).value
     lat = ws_our_data.cell(row=row,column=7).value
     long = ws_our_data.cell(row=row,column=8).value
-    address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}, {lat} {long}'
+    address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}; {lat} {long}'
+    #address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}'
     list_our_data.append(address_full.lower())
 
 # Repeat above, but for corning data.
@@ -60,7 +61,8 @@ for row in range(start_row, end_row+1):
     zip_code = ws_corning_data.cell(row=row,column=6).value
     lat = ws_corning_data.cell(row=row,column=7).value
     long = ws_corning_data.cell(row=row,column=8).value
-    address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}, {lat} {long}'
+    address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}; {lat} {long}'
+    #address_full = f'{mail_add} unit {unit_number}, {municipality} {zip_code}'
     list_corning_data.append(address_full.lower())
 
 # Test to check the length our list. Should be 8,262.
@@ -82,10 +84,67 @@ print('Sample output:')
 print('--------------------------')
 for i in range(0,5):
     print(list_corning_data[i])
+print('--------------------------')
 # Checks on the initial list output are complete.
 
 # Store a sorted copy of each list.
 list_sort_our_data = sorted(list_our_data)
 list_sort_corning_data = sorted(list_corning_data)
+"""
+# Test to see if the script can find mathing
+# addresses
+i = 0
+for address in list_sort_our_data:
+    if address in list_sort_corning_data:
+        print(f'Match found for: {address}')
+        i += 1
+        if i > 10:
+            break
+print('--------------------------')
+"""
+# Initial variables for matched 
+# and unmatched lists.
+list_unmatched_addresses = []
+list_matched_addresses_our_data = []
+
+# Initial variables to store lists with no lat and long.
+list_sort_split_our_data = []
+list_sort_split_corning_data = []
+
+# Populate list_sort_split_our_data
+for address in list_sort_our_data:
+    list_sort_split_our_data.append(address.split(';')[0])
+    #print(address.split(';')[0])
+
+# Populated the list_sort_split_corning_data
+for address in list_sort_corning_data:
+    list_sort_split_corning_data.append(address.split(';')[0])
+
+# Iterate through the sorted lists.
+# to look for matching addresses.
+# Store both matched and unmatched addresses.
+# If an address is matched, also remove it from 
+# the sorted list of corning data.
+for index, address in enumerate(list_sort_split_our_data):
+    if address in list_sort_split_corning_data:
+        index_corning_data = list_sort_split_corning_data.index(address)
+        # Append matched address to list_matched_address.
+        list_matched_addresses_our_data.append(list_sort_our_data[index])
+        # Remove matched address from corning data.
+        list_sort_corning_data.remove(list_sort_corning_data[index_corning_data])
+        list_sort_split_corning_data.remove(address)
+    else:
+        list_unmatched_addresses.append(list_sort_our_data[index])
+
+print(f'Length of matched addresses: {len(list_matched_addresses_our_data)}')
+print(f'Length of un-matched addresses: {len(list_unmatched_addresses)}')
+print(f'Length of corning addresses: {len(list_sort_corning_data)}')
+print('--------------------------')
+
+# Go through the unmatched address list,
+# matched address list, and sorted
+# corning data and write it to a new excel file.
+wb_output = openpyxl.Workbook()
+
 
 
